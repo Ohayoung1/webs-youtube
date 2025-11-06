01ad674766dc6f882d47940e4e9467b12b149fc3
Ohayoung1 <dhgkdud114@naver.com>
2025-11-06 11:18:07 +0900

    섹션 컴퍼넌트 구조화 하기"

---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <header id='footer' role='contentinfo'>
+            footer
+        </header>
+    )
+}
+
+export default Footer
---
+++ src/components/section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <header id='main' role='main'>
+            main
+        </header>
+    )
+}
+
+export default main
