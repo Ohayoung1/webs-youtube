b06c0e1bd15edaf2e9ba31bffbf14407386f61a0
Ohayoung1 <dhgkdud114@naver.com>
2025-11-06 14:50:57 +0900

    페이지 SEO 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -4,6 +4,8 @@ import Main from '../components/section/Main'
 const Channel = () => {
     return (
         <Main>
+            title = "유튜브 채널"
+            description="유튜브 채널페이지입니다."
             Channel
         </Main>
     )
