0611e32d6d1d714257b53742dd6e6909727c5cbf
Ohayoung1 <dhgkdud114@naver.com>
2025-11-10 12:15:06 +0900

    메인 콘텐츠 작업

---
+++ src/components/contents/Website.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div>
+      Webd
+    </div>
+  )
+}
+
+export default Website
--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <header id='footer' role='contentinfo'>
-            footer
-        </header>
+         <footer id='footer' role="contentinfo"> 
+            <a href="mailto:dhgkdud114@naver.com" rel="noopener noreferrer">
+                dhgkdud114@naver.com
+            </a>
+        </footer>
     )
 }
 
