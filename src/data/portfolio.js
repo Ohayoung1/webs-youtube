40a047ae549efcb515020466fce46c83c6661162
Ohayoung1 <dhgkdud114@naver.com>
2025-11-11 20:08:30 +0900

    메인 섹션 나머지 콘텐츠 작업

---
+++ src/data/portfolio.js
@@ -0,0 +1,84 @@
+import port01 from "../assets/img/port/port-next01.jpg";
+import port02 from "../assets/img/port/port-next02.jpg";
+import port03 from "../assets/img/port/port-next03.jpg";
+import port04 from "../assets/img/port/port-next04.jpg";
+import port05 from "../assets/img/port/port-next05.jpg";
+import port06 from "../assets/img/port/port-next06.jpg";
+import port07 from "../assets/img/port/port-next07.jpg";
+import port08 from "../assets/img/port/port-next08.jpg";
+import port09 from "../assets/img/port/port-next09.jpg";
+import port10 from "../assets/img/port/port-next10.jpg";
+
+export const portfolioText = [
+    {
+        title: "01. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 셋팅하기 | 웹스토리보이 | 포트폴리오",
+        img: port01,
+        author: "webstoryboy",
+        videoId: "2ZiZlEvtWgM",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "02. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 레이아웃 | 프론트앤드 | 포트폴리오",
+        img: port02,
+        author: "webstoryboy",
+        videoId: "qEHaq4M2lXs",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "03. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 헤더 영역 | 웹스토리보이 | 포트폴리오",
+        img: port03,
+        author: "webstoryboy",
+        videoId: "fzH2LsKoVdg",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "04. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 인트로 영역 | 웹스토리보이 | 리액트",
+        img: port04,
+        author: "webstoryboy",
+        videoId: "N4itpjUEqss",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "05. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 스킬 영역 | 웹스토리보이 | 비트",
+        img: port05,
+        author: "webstoryboy",
+        videoId: "8FemQEZVAyQ",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "06. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 사이트 영역 | 웹스토리보이 | 포폴",
+        img: port06,
+        author: "webstoryboy",
+        videoId: "HoMX0lQfzvg",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "07. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 포트폴리오 영역 | 웹스토리보이 | 자바스크립트",
+        img: port07,
+        author: "webstoryboy",
+        videoId: "AEVpBAeXOBQ",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "08. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 연락처 영역 | 웹스토리보이 | 퍼블리셔",
+        img: port08,
+        author: "webstoryboy",
+        videoId: "mjZISLURugw",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "09. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 푸터 영역 | 웹스토리보이 | 사이트제작",
+        img: port09,
+        author: "webstoryboy",
+        videoId: "r2Gv5a_SJcY",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "10. 다양한 프레임워크로 구축하는 포트폴리오 사이트 개발 : next : 스킬 영역 | 웹스토리보이 | 비트",
+        img: port10,
+        author: "webstoryboy",
+        videoId: "ePvRxd2d4Ss",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }
+]
